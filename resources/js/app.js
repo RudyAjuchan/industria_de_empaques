6f3830917c438a40e1267e7c701e2f0823b940b8
Rudy Ajuchán <rudyajuchan@MacBook-Air-de-Rudy.local>
2026-06-13 13:03:41 -0600

    cambios errores

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,12 @@ import axios from 'axios'
 window.axios = axios
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
+const csrfToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content')
+
+if (csrfToken) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+}
+
 // Vuetify
 import '@mdi/font/css/materialdesignicons.css'
 import '@fortawesome/fontawesome-free/css/all.css'
